e4093c6830cc2309b2030bb29c00f0d98fbfea41
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-11 17:48:33 +0900

    Create LoginService.java
    feat LoginService add

---
+++ src/main/java/com/example/demo/login/service/LoginService.java
@@ -0,0 +1,28 @@
+package com.example.demo.login.service;
+
+import com.example.demo.login.domain.User;
+import com.example.demo.login.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    public User authenticateUser(String email, String password) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (user.getPassword().equals(password)) {
+                return user; // 인증 성공 시 User 객체 반환
+            }
+        }
+
+        return null; // 인증 실패 시 null 반환
+    }
+}
