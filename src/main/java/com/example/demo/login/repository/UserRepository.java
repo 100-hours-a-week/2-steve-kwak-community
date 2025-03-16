2a3727c78be6ae01a3d295e27356fb2f6e03f60e
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-16 17:37:54 +0900

    Update UserRepository.java
    feat UserRepositry fix

--- src/main/java/com/example/demo/login/repository/UserRepository.java
+++ src/main/java/com/example/demo/login/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.login.repository;
 
 import com.example.demo.login.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email); // 이메일로 회원 조회
 }
