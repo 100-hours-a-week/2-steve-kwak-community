f01a984a234e33b8258c6208360f3d0bf0b02cc7
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-17 13:30:07 +0900

    Update UserRepository.java
    feat UserRepository fix

--- src/main/java/com/example/demo/login/repository/UserRepository.java
+++ src/main/java/com/example/demo/login/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email); // 이메일로 회원 조회
+    User findById(long id);
 }
