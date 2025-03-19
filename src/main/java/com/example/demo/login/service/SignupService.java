11f63111a9510c0e66fed46d0dcb33f659586465
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-19 10:54:21 +0900

    Update SignupService.java

--- src/main/java/com/example/demo/login/service/SignupService.java
+++ src/main/java/com/example/demo/login/service/SignupService.java
@@ -2,20 +2,27 @@ package com.example.demo.login.service;
 
 import com.example.demo.login.domain.User;
 import com.example.demo.login.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class SignupService {
 
     private final UserRepository userRepository;
 
-    public SignupService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Transactional
     public User registerUser(User user) {
-        return userRepository.save(user);
+        // 중복 이메일 검사
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+
+        try {
+            return userRepository.save(user);
+        } catch (Exception e) {
+            throw new RuntimeException("회원가입 중 오류가 발생했습니다.");
+        }
     }
 }
