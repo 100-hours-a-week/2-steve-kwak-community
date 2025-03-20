c5171ee8e8d7907b62229a045c7f73d17209fd95
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-20 13:02:01 +0900

    Update Comment.java

--- src/main/java/com/example/demo/post/domain/Comment.java
+++ src/main/java/com/example/demo/post/domain/Comment.java
@@ -2,6 +2,7 @@ package com.example.demo.post.domain;
 
 import com.example.demo.login.domain.User;
 import com.example.demo.post.domain.Post;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,11 +23,13 @@ public class Comment {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = true)
+    @JsonBackReference
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "post_id", nullable = false)
+    @JsonBackReference
     private Post post;
 
     @Column(nullable = false, columnDefinition = "TEXT")
