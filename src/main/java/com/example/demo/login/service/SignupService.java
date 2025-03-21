896a04efc87ef0acc621d4467174d5e3cb23daa4
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-21 16:15:09 +0900

    Update SignupService.java

--- src/main/java/com/example/demo/login/service/SignupService.java
+++ src/main/java/com/example/demo/login/service/SignupService.java
@@ -5,12 +5,16 @@ import com.example.demo.login.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @RequiredArgsConstructor
 public class SignupService {
 
     private final UserRepository userRepository;
+    private static final Logger log = LoggerFactory.getLogger(SignupService.class);
+
 
     @Transactional
     public User registerUser(User user) {
@@ -22,6 +26,7 @@ public class SignupService {
         try {
             return userRepository.save(user);
         } catch (Exception e) {
+            log.error("회원가입 오류: ", e);
             throw new RuntimeException("회원가입 중 오류가 발생했습니다.");
         }
     }
