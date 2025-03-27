c04d8a17e81ed84be5fb6711e34706cba81482ef
dyk000825 <120299786+dyk000825@users.noreply.github.com>
2025-03-27 13:13:20 +0900

    Update PostLikeService.java

--- src/main/java/com/example/demo/post/service/PostLikeService.java
+++ src/main/java/com/example/demo/post/service/PostLikeService.java
@@ -19,7 +19,6 @@ public class PostLikeService {
     public int getLikeCountByPostId(Long postId) {
         return postLikeRepository.countByPostId(postId); // 해당 postId에 대한 좋아요 수 반환
     }
-
     // 게시글 좋아요 처리
     @Transactional
     public void likePost(Long postId, User user) {
@@ -38,9 +37,12 @@ public class PostLikeService {
                 .user(user)
                 .build();
         postLikeRepository.save(postLike);
+
         // 게시글의 좋아요 수 증가
-        post.setLikeCount(post.getLikeCount() + 1);  // likeCount 증가
+        post = post.increaseLikeCount();  // likeCount 증가 후 반환된 새로운 객체를 post에 할당
+
         postRepository.save(post);  // 변경된 게시글 저장
     }
+
 
 }
